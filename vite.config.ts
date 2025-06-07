e322abf3aa7c5c56d572f17bed2af205f7c8a508
dy rongrath <Dy-Rongrath@users.noreply.github.com>
2025-06-07 16:38:09 +0700

    fix error when deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/Falcon-Admin-Dashboard-WebApp-Template/",
   plugins: [react()],
   resolve: {
     alias: {
