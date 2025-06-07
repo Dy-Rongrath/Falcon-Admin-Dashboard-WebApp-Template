207124170c6bbc87b2ed8e06810357a3480e993b
dy rongrath <Dy-Rongrath@users.noreply.github.com>
2025-06-07 15:07:25 +0700

    add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Falcon-Admin-Dashboard-WebApp-Template/",
   server: {
     host: "::",
     port: 8080,
